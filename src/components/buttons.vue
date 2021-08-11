5fd376a557c13921155af6520b83f4739482f045
rukayyah-sayid <41972876+rukayyah-sayid@users.noreply.github.com>
2021-08-11 16:46:03 +0100

    initial commit

---
+++ src/components/buttons.vue
@@ -0,0 +1,30 @@
+<template>
+ <div class="mt-2">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+<div class="d-inline">
+  <button class="btn pt-3 pb-3">
+    <i class="fa fa-search rounded-left mr-2"></i>Job Title or Keyword
+    </button>
+    <button class="btn pt-3 pb-3">
+    <i class="fa fa-map-marker rounded-0 mr-2"></i>Job Title or Keyword
+    </button>
+    <button class="btn btn-danger rounded-right pt-3 pb-3">Search</button>
+</div>
+ </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+@import url("https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap");
+
+button {
+  font-family: 'DM Sans', sans-serif;
+  font-size: 12px;
+}
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container-fluid p-0 m-0">
+    <ul class="col-lg-12 list-unstyled list-inline pl-5 mt-4">
+      <li class="mr-5 ml-3 list-inline-item">
+        <img src="../assets/Jobply.png" width="80" alt="">
+        </li>
+        <li class="mr-3 list-inline-item">
+        <a href="#">
+          Home
+          </a>
+        </li>
+        <li class="mr-3 list-inline-item">
+        <a href="#">
+          Categories
+        </a>
+        </li>
+        <li class="mr-3 list-inline-item">
+        <a href="#">
+          Services
+        </a>
+        </li>
+        <li class="mr-3 list-inline-item">
+        <a href="#">
+          Jobs
+        </a>
+        </li> 
+        <li class="float-right list-inline-item mr-5">
+        <button class="btn btn-danger ml-5 pl-4 pr-4">Signup</button>
+        </li>
+    </ul>
+    <pictures/>
+  </div>
+</template>
+
+<script>
+import pictures from './pictures'
+export default {
+  name: 'navbar',
+  components: {
+    pictures
+  }
+}
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap");
+li a {
+  font-family: 'DM Sans', sans-serif;
+  font-size: 15px;
+  font-weight: normal;
+  color: #232443;
+}
+
+button {
+  font-family: 'DM Sans', sans-serif;
+  font-size: 12px;
+  background: #212125;
+  border: none;
+}
+</style>
